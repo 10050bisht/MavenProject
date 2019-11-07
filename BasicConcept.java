d5dbf8b0f39fe7f76d165fa61d445b1819dcb94e
10050bisht <57168251+10050bisht@users.noreply.github.com>
2019-11-07 13:53:12 +0530

    AgainChnages

--- BasicConcept.java
+++ BasicConcept.java
@@ -1,11 +1,6 @@
-package mvnConcepts;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
 
 public class BasicConcept {
-	WebDriver driver;
+	
 	 
 	public static void main(String[] args)
 	{
